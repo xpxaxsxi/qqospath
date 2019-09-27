c19d36fc13f855439e61dc9a8a087bb4b30d1fc7
xpxaxsxi <pasikuuskasi@windowslive.com>
2019-09-27 19:23:08 +0300

    A heavy duty version
    Caches results when appropriate

--- qqospath.pl
+++ qqospath.pl
@@ -1,9 +1,51 @@
-:- module(qqpospath, [ospath/4]).
+:- module(qqospath, [ospath/4,command/2,cache_count/0]).
 :- use_module(library(quasi_quotations)).
 :- quasi_quotation_syntax(ospath).
 
+:- dynamic dict_result_cached/4.
+:- use_module(sources_root(esleep)).
+
+%for debugging shows a window that tells number of cached items
+cache_count:-
+    thread_create((attach_console,thread_self(A),atomic_concat(we_,A,Eng),restart_eng(Eng),repeat,aggregate_all(count,dict_result_cached(_,_,_,_),R),
+    write(R),tab(2),flush_output,\+next(Eng,0.5)),_,[alias(cache_count),detached(true)]).
+
+command(QQ,Res):-
+    atomic_list_concat(QQ,Res).
+
+%if nothing has changed take the Result from cache
 ospath(Content,SyntaxArgs,VariableNames,Result):-
-    phrase_from_quasi_quotation(path(SyntaxArgs,VariableNames,Result), Content).
+    with_mutex(qqospath,(clear_cache(5.0),
+    must_be(list, VariableNames),
+    include(qq_var(SyntaxArgs), VariableNames, QQDict),
+
+    (   \+current_prolog_flag(xref,true),
+        %asserta(no_xref),
+        dict_result_cached(Content,QQDict,TmpRes,_),TmpRes=Result,!,
+        true %garbage_collect
+    ;
+        %asserta(xref),
+    phrase_from_quasi_quotation(path(SyntaxArgs,QQDict,Result), Content),
+        retractall(dict_result_cached(Content,_,_,_)),
+        get_time(Now),
+        asserta(dict_result_cached(Content,QQDict,Result,Now)),
+         true %garbage_collect
+    ))).
+
+%clears items that are too old
+clear_cache(TimeSeconds):-
+    get_time(Now),
+    MinTime is Now - TimeSeconds,
+    forall((dict_result_cached(K,L,M,WhenAsserted),
+    WhenAsserted < MinTime),
+    retractall(dict_result_cached(K,L,M,WhenAsserted))),!,
+    garbage_collect.
+
+
+clear_cache(_).
+
+%filter out variables that are not in between brackets
+qq_var(Vars, _=Var) :- member(V, Vars), V == Var, !.
 
 
 
@@ -13,31 +55,41 @@ path(SA,VN,[H|T]) -->
     variable(SA,VN,Var),!,{H=Var},
     path(SA,VN,T).
 
-path(SA,VN,[H|T]) -->
-    pathsegment(SA,VN,Seg),!,{atomic_list_concat(Seg,SegA),H=SegA},
+path(SA,VN,[H|T] ) -->
+    [HCode], {atom_codes(H,[HCode])},!,
     path(SA,VN,T).
 
 
-
-
 variable(SA,VN,A)  -->
-    {member(A,SA),member(VarName=B,VN),A==B,atom_codes(VarName,VNCodes)},
-
-    VNCodes,!.
-
-slash(_,_,A) -->
-    [0'\\],{A='\\'}.
-
-pathatom(_,_,A) --> slash(_,_,A),!.
-pathatom(_,_,A) -->
-     [H2],{atom_codes(A,[H2])}.
+    {member(VarName=B,VN),atom_codes(VarName,VNCodes)},
+    VNCodes,!,
+    {member(A,SA),A==B}.
 
 
-pathsegment(SA,VN,[H|T]) -->
-    \+variable(SA,VN,_),
-    pathatom(_,_,H),!,
-    pathsegment(SA,VN,T).
-pathsegment(_SA,_VN,[],A,A).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
